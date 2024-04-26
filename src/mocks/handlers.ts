c6c7e84e66ac9f08a73b84bf30abef6095b6748d
bitbravo <jsdev63@yahoo.com>
2024-04-25 20:46:56 -0600

    Add test user credential

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,4 +1,4 @@
-import { delay, http, HttpResponse } from 'msw';
+import { delay, http, HttpResponse } from "msw";
 import {
   AuthorResponse,
   InfoResponse,
@@ -6,93 +6,98 @@ import {
   LoginResponse,
   ProfileResponse,
   QuoteResponse,
-} from '../types/api';
+} from "../types/api";
+import { TestUser } from "../config/constants";
 
 export const handlers = [
-  http.get('/info', () => {
+  http.get("/info", () => {
     const response: InfoResponse = {
       success: true,
-      data: { info: 'Some information about the <b>company</b>.' },
+      data: { info: "Some information about the <b>company</b>." },
     };
     return HttpResponse.json(response);
   }),
 
-  http.post<Record<string, never>, LoginRequest, LoginResponse>(
-    '/login',
-    async ({ request }) => {
-      const { email, password } = await request.json();
-      if (!email) {
-        return HttpResponse.json({
-          success: false,
-          data: { message: 'Email is required' },
-        });
-      }
-
-      if (!password) {
-        return HttpResponse.json({
-          success: false,
-          data: { message: 'Password is required' },
-        });
-      }
-
-      const response = {
-        success: true,
-        data: { token: 'fb566635a66295da0c8ad3f467c32dcf' },
-      };
-      return HttpResponse.json(response);
-    },
-  ),
-
-  http.get('/profile', ({ request }) => {
-    const url = new URL(request.url);
-    const token = url.searchParams.get('token');
-    if (!token || token === 'null') {
+  http.post<Record<string, never>, LoginRequest, LoginResponse>("/login", async ({ request }) => {
+    const { email, password } = await request.json();
+    if (!email) {
       return HttpResponse.json({
         success: false,
-        data: { message: 'Token is required' },
+        data: { message: "Email is required" },
+      });
+    }
+
+    if (!password) {
+      return HttpResponse.json({
+        success: false,
+        data: { message: "Password is required" },
+      });
+    }
+
+    if (email !== TestUser.email || password !== TestUser.password) {
+      return HttpResponse.json({
+        success: false,
+        data: { message: "Invalid email or password" },
+      });
+    }
+
+    const response = {
+      success: true,
+      data: { token: "fb566635a66295da0c8ad3f467c32dcf" },
+    };
+    return HttpResponse.json(response);
+  }),
+
+  http.get("/profile", ({ request }) => {
+    const url = new URL(request.url);
+    const token = url.searchParams.get("token");
+    if (!token || token === "null") {
+      return HttpResponse.json({
+        success: false,
+        data: { message: "Token is required" },
       });
     }
     const response: ProfileResponse = {
       success: true,
-      data: { fullname: 'Aleksei K', email: 'aleksei@example.com' },
+      data: { fullName: "Aleksei K", email: "aleksei@example.com" },
     };
     return HttpResponse.json(response);
   }),
 
-  http.get('/author', async ({ request }) => {
+  http.get("/author", async ({ request }) => {
     const url = new URL(request.url);
-    const token = url.searchParams.get('token');
+    const token = url.searchParams.get("token");
     if (!token) {
       return HttpResponse.json({
         success: false,
-        data: { message: 'Token is required' },
+        data: { message: "Token is required" },
       });
     }
     const response: AuthorResponse = {
       success: true,
-      data: { authorId: 1, name: 'Charlie Chaplin' },
+      data: { authorId: 1, name: "Charlie Chaplin" },
     };
     await delay(3000);
 
     return HttpResponse.json(response);
   }),
 
-  http.get('/quote', async ({ request }) => {
+  http.get("/quote", async ({ request }) => {
     const url = new URL(request.url);
-    const token = url.searchParams.get('token');
-    const authorId = url.searchParams.get('authorId');
+    const token = url.searchParams.get("token");
+    const authorId = url.searchParams.get("authorId");
 
     if (!token) {
       return HttpResponse.json({
         success: false,
-        data: { message: 'Token is required' },
+        data: { message: "Token is required" },
       });
     }
 
     if (!authorId) {
       return HttpResponse.json({
         success: false,
-        data: { message: 'authorId is required' },
+        data: { message: "authorId is required" },
       });
     }
     await delay(3000);
@@ -102,13 +107,13 @@ export const handlers = [
       data: {
         quoteId: 1,
         authorId: 1,
-        quote: 'A day without laughter is a day wasted.',
+        quote: "A day without laughter is a day wasted.",
       },
     };
     return HttpResponse.json(response);
   }),
 
-  http.delete('/logout', () => {
+  http.delete("/logout", () => {
     return HttpResponse.json({
       success: true,
       data: {},
