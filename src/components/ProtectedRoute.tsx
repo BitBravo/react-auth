d921fbc79f9e501f066114ec87407e77e311869c
bitbravo <jsdev63@yahoo.com>
2024-04-26 01:51:29 -0600

    fixed issue

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,13 +1,15 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth';
-import { RoutePaths } from '../config/routes';
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+import { RoutePaths } from "../config/routes";
 
 const ProtectedRoute = () => {
   const { loading, isAuthenticated } = useAuth();
 
   if (loading) {
     return <>Loading...</>;
-  } else if (!isAuthenticated) {
+  }
+
+  if (!isAuthenticated) {
     return <Navigate to={RoutePaths.Info} replace />;
   }
 
